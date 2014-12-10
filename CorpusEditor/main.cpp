b3f681f203b34f64eea5f860199d130c0e891b75
Marco Zille <marco.zille@gmail.com>
2014-12-10 11:15:22 +0100

    Risolto problema Corpus su Mac
    Risolto problema che se si installava Corpus tramite la compilazione
    INSTALL, il programma non partiva

--- CorpusEditor/main.cpp
+++ CorpusEditor/main.cpp
@@ -7,8 +7,39 @@
 #include <QStyleFactory>
 #include "CorpusMainWindow.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+	freopen ("/Users/marco/Desktop/logs/CorpusEditor.log", "a", stderr);
+	
+	QByteArray localMsg = msg.toLocal8Bit();
+	switch (type) {
+		case QtDebugMsg:
+			fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+		case QtWarningMsg:
+			fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+		case QtCriticalMsg:
+			fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+		case QtFatalMsg:
+			fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			abort();
+	}
+	
+	fclose (stderr);
+}
+
 int main(int argc, char** argv)
 {
+	FILE* f = fopen("/Users/marco/Desktop/logs/CorpusEditor.log", "w");
+	fclose(f);
+	
+	qInstallMessageHandler(myMessageOutput);
+	
 	QApplication app(argc, argv);
 	
 	qApp->setStyle(QStyleFactory::create("fusion"));
